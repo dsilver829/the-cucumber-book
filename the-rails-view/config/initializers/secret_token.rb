5bfa3e598b7fe78f24a326ffdd26c844f733ae27
David Silver <dsilver@candidatemetrics.com>
2012-04-07 20:32:35 -0400

    initializing project

---
+++ the-rails-view/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRailsView::Application.config.secret_token = '05b66bab02e73d09beef7e6184cba286dce3bce467eae1c8c782cf5da866bb9aca1a1826a3b27cf6a2a7fff5c74d8fea7d3eb79a9d582d5392f248de7d6389b3'
---
+++ the-rails-view/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TheRailsView::Application.config.session_store :cookie_store, key: '_the-rails-view_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TheRailsView::Application.config.session_store :active_record_store
